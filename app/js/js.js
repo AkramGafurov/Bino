5e0f30ac1aeee3463955bdc1063eaf3a9f1567c7
AkramGafurov <akramminsk@yander.ru>
2020-03-22 13:58:52 +0300

    adding sliders

--- app/js/js.js
+++ app/js/js.js
@@ -1,9 +1,100 @@
 $(document).ready(function() {
   $(".header__slider").slick({
-    //speed: 1500,
+    speed: 1500,
+    autoplay: true
+  });
+
+  $(window).on("load resize", function() {
+    var width = $(document).width();
+
+    if (width > 1024) {
+      $(".stats__slider").slick("unslick");
+    } else {
+      $(".stats__slider")
+        .not(".slick-initialized")
+        .slick({
+          arrows: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          speed: 1000,
+          autoplay: true,
+          responsive: [
+            {
+              breakpoint: 670,
+              settings: {
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                speed: 1000,
+                autoplay: true
+              }
+            },
+            {
+              breakpoint: 414,
+              settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                speed: 1000,
+                autoplay: true
+              }
+            }
+          ]
+        });
+    }
+  });
+  //.pricing__slider
+  $(window).on("load resize", function() {
+    var width = $(document).width();
+
+    if (width > 768) {
+      $(".pricing__slider").slick("unslick");
+    } else {
+      $(".pricing__slider")
+        .not(".slick-initialized")
+        .slick({
+          arrows: false,
+          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          speed: 1000,
+          responsive: [
+            {
+              breakpoint: 670,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                speed: 1000
+              }
+            },
+            {
+              breakpoint: 414,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                speed: 1000
+              }
+            }
+          ]
+        });
+    }
+  });
+
+  $(".case-study__slider").slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    speed: 1000
     //autoplay: true
   });
-  $(".case-study__slider").slick({});
 
   $(".nav__mobile-menu").click(function(e) {
     $(".nav__mobile-menu").toggleClass("nav__line-active");
